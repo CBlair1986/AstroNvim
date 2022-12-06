625dc3098d4c51fdaa8c07737fbb496e50864003
harblcat <CBlair1986@gmail.com>
2022-12-06 12:42:04 -0500

    forked

---
+++ lua/user/init.lua
@@ -0,0 +1,17 @@
+local config = {
+  options = {
+    opt = {
+      guifont = "PragmataPro Liga:h14",
+    }
+  },
+  plugins = {
+    init = {
+      {
+        "Mofiqul/dracula.nvim",
+      },
+    }
+  },
+  colorscheme = "dracula",
+}
+
+return config
